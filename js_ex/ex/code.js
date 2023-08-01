4cfb005f453c61e775749bee50f0e0174008bb09
“so49861” <so49861@gmail.com>
2023-08-01 10:05:22 +0900

    練習

--- js_ex/ex/code.js
+++ js_ex/ex/code.js
@@ -1,10 +1,14 @@
 const labelElement = document.getElementById("text-result");
 let value = 0;
 function updateLabel() {
-    labelElement.textContent = `値：`;
+    labelElement.textContent = `値：${value}`;
 }
 document.getElementById("change-text1").addEventListener("click", () => {
     value = 0;
+    updateLabel();
+});
+document.getElementById("change-text2").addEventListener("click", () => {
+    value += 1;
     updateLabel();
 });
 document.getElementById("change-text3").addEventListener("click", () => {
